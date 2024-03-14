dcda82ab5d157ce31da2fe8e4bc9ea91f8aca164
Adorno7117 <ga7.adorno@outlook.com>
2024-03-14 14:53:26 -0300

    att14.04

--- main.py
+++ main.py
@@ -1,46 +1,70 @@
-# OBIJETIVO: adiconar,bloquear,desbloquear, excluir uma url
-
 import sys
-from PyQt5 import QtWidgets
-from ui_home import Ui_bloq 
-from PyQt5.QtWidgets import QMessageBox
+import os
+from PyQt5 import QtCore, QtGui, QtWidgets, uic
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QMessageBox
 
-class Princpal (QtWidgets.QMainWindow, Ui_bloq):
-    def __init__(self, parent=None):
-        super(Princpal, self).__init__(parent)
-        self.setupUi(self)
-        self.setWindowTitle("Sistema bloqueador de sites")
-        
-        #botões
+from ui_home import Ui_Form
+
+class MainWindow(QtWidgets.QWidget, Ui_Form):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('ui_home.ui', self)
+        self.setWindowTitle("Bloqueador de Sites") 
+
+        # Conectar os botões aos slots
         self.bt_adicionar.clicked.connect(self.adicionaUrl)
         self.bt_bloquear.clicked.connect(self.bloqueiaUrl)
         self.bt_desbloquear.clicked.connect(self.desbloqueiaUrl)
         self.bt_excluir.clicked.connect(self.excluiUrl)
 
-
+        self.treeWidget.itemSelectionChanged.connect(self.selecionaUrl)
 
     def adicionaUrl(self):
+        # Obter a URL do QLineEdit
         url = self.input_site.text()
+
+
         if url:
-            self.listWidget_urls.addItem(url)
+            item = QtWidgets.QTreeWidgetItem([url])
+            item.setText(0, url)
+            self.treeWidget.addTopLevelItem(item)
+            self.input_site.clear()
+
         else:
             QtWidgets.QMessageBox.warning(self, "Aviso", "Por favor, insira uma URL válida")
 
     
-    def bloqueiaUrl(self):
-        pass
+    def selecionaUrl(self):
+        selected_item = self.treeWidget.currentItem()
 
-    
+        if selected_item:
+            self.input_site.setText(selected_item.text(0))
+
+
+
+    def bloqueiaUrl(self):
+        selected_item = self.treeWidget.currentItem()
+
+        if selected_item:
+            with open(r'C:\\Windows\\System32\\drivers\\etc\\hosts', 'a') as file:
+                file.write(f'\n127.0.0.1 {url}')
+
+            bloquearUrl('exemplo.com')
+
     def desbloqueiaUrl(self):
         pass
-    
-    def excluiUrl(self):
-        pass
 
+    def excluiUrl(self):
+        selected_item = self.treeWidget.currentItem()
+
+        if selected_item:
+            
+            index = self.treeWidget.indexOfTopLevelItem(selected_item)
+
+            self.treeWidget.takeTopLevelItem(index)
 
 if __name__ == "__main__":
-    import sys
-    app = QtWidgets.QApplication(sys.argv)
-    window = Princpal()
+    app = QApplication(sys.argv)
+    window = MainWindow()
     window.show()
     sys.exit(app.exec_())
